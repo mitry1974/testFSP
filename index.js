0764e6de9156eac243a5f6b8ad1377764df7283d
DT <tomitry@gmail.com>
2020-10-27 15:18:48 +0300

    move all code to test

--- index.js
+++
@@ -1,9 +0,0 @@
-import fastify from 'fastify';
-const fastifySequelize = require('fastify-sequelize');
-const config = require('./dbconfig');
-
-export default async () => {
-  const app = fastify();
-  app.register(fastifySequelize, config);
-  return app;
-}
